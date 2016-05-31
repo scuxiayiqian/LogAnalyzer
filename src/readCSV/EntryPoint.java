601d37242a12838dcda72b378d2dc43e1e3bdd97
scuxiayiqian <scuxiayiqian@gmail.com>
2016-05-31 15:39:15 +0800

    reset

--- src/readCSV/EntryPoint.java
+++ src/readCSV/EntryPoint.java
@@ -13,13 +13,13 @@ public class EntryPoint {
 		CsvReader csvReader;		
 		
 		if (args.length == 0) {
-			csvReader = new CsvReader("/Users/xiayiqian/Downloads/data.csv", "/Users/xiayiqian/Downloads/output/");
+			csvReader = new CsvReader("/Users/xiayiqian/Downloads/data.csv", "/Users/xiayiqian/Downloads/output41/");
 		}
 		else {
 			csvReader = new CsvReader(args[0], args[1]);
 		}
 		
-		csvReader.setTimes(100000);  // 设置1 - 统计的数据有多少条
+		csvReader.setTimes(273184);  // 设置1 - 统计的数据有多少条
 		
 		csvReader.read();
 		csvReader.handleDatas();
@@ -30,8 +30,8 @@ public class EntryPoint {
 		SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
 		Date timepoint;
 		try {
-			timepoint = df.parse("2016-03-11 07:07:56");  // 设置2 - 从哪个时间点开始分析次数
-			csvReader.collectRSTimes(timepoint, 300000);   // 设置3 - 分析多久时间内的次数
+			timepoint = df.parse("2016-03-15 16:04:36");  // 设置2 - 从哪个时间点开始分析次数
+			csvReader.collectRSTimes(timepoint, 1200000);   // 设置3 - 分析多久时间内的次数
 		} catch (ParseException e) {
 			e.printStackTrace();
 			System.out.println("in parse exception");
